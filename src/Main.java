caf2d6245eb2b4293aa53057914dda31911a5b62
corp.dev.vtb\VTB4059432 <dosafin@vtb.ru>
2024-02-27 15:59:11 +0500

    "Вычисление двух чисел"

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма: Первое+второе= "+(firstNumber+secondNumber));
+        System.out.println("Разность: Первое-второе= "+(firstNumber-secondNumber));
+        System.out.println("Произведение: Первое*второе= "+(firstNumber*secondNumber));
+        System.out.println("Частное: Первое/второе= "+quotient);
+
     }
 }
